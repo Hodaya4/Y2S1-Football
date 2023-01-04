3e4c0d8e1241d839c980aae905df0193c202a2d8
Hodaya Gamliel <Hodaya4799@gmail.com>
2023-01-04 23:48:04 +0200

    tables and history in progress

--- src/ResultsHistory.js
+++ src/ResultsHistory.js
@@ -1,7 +1,74 @@
-function ResultsHistory() {
-    return(
-        <div>ResultsHisoty</div>
-    )
+import React from "react";
+import axios from "axios";
+import SelectBox from "./SelectBox";
+
+class ResultsHistory extends React.Component {
+
+    state = {
+        startRound: 1,
+        endRound: 0,
+        roundList: [],
+        leagues: []
+
+    }
+
+    componentDidMount = () => {
+        axios.get("https://app.seker.live/fm1/history/" + this.state.leagueId)
+            .then((response) => {
+                const lastGame = response.data[response.data.length - 1]
+                this.setState({
+                    endRound: lastGame.round
+                })
+            })
+        this.roundList();
+    }
+
+    roundList = () => {
+        let tempList = []
+        for (let i = this.state.startRound; i <= this.state.endRound; i++) {
+            tempList.push(i)
+        }
+        this.setState({
+            roundList : tempList
+        })
+    }
+
+    startRoundChanged = (event) => {
+        this.setState({
+            startRound: event.target.value
+        })
+
+
+    }
+
+    setLeagues = (receivedLeagues) => {
+        this.setState({
+            leagues: receivedLeagues
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <SelectBox responseClick={this.setLeagues.bind(this)}/>
+                Start:
+                <select value={this.state.startRound} onChange={this.startRoundChanged}>
+                    <option value={"none"} disabled={true}>Select Start Round</option>
+                    {
+                        this.state.roundsList.map((item) => {
+                            return (
+                                <option value={item}>{item}</option>
+                            )
+                        })
+                    }
+                </select>
+
+
+            </div>
+        )
+    }
+
+
 
 }
 
--- src/Tables.js
+++ src/Tables.js
@@ -1,106 +1,173 @@
 import "./Tables.css";
 import axios from "axios";
 import React from "react";
+import SelectBox from "./SelectBox";
+
 class Tables extends React.Component {
 
     state = {
-        playerNames: []
+        playerNames: [],
+
+        teams: []
     }
+    tempTeams = []
+
     arrPlayerNames = []
 
-    /*calcEachTeamPoints(games) {
-        let tempGames = []
-        games.forEach(game => {
-            const homeTeam = game.homeTeam.name
-            let homeTeamGoals = 0
-            const awayTeam = game.awayTeam.name
-            let awayTeamGoals = 0
-            const goals = game.goals
-            goals.forEach(goal => {
-                if(goal.home) {
-                    homeTeamGoals++
-                }
-                else {
-                    awayTeamGoals++
-                }
-            })
-            tempGames.push({homeTeam : homeTeam, homeTeamGoals : homeTeamGoals, awayTeam : awayTeam, awayTeamGoals : awayTeamGoals})
-        })
-        this.setState({
-            games : tempGames
-        })
-    }*/
 
-    /*setTeamPlayers(team) {
-        let tempPlayers = []
-        team.forEach(player => {
-            tempPlayers.push(player.firstName + " " + player.lastName)
-        })
-        this.setState({team : tempPlayers})
-    }
+    getTeams = (currentLeagueId) => {
+            axios.get("https://app.seker.live/fm1/teams/" + currentLeagueId)
+                .then((response) => {
+                    this.tempTeams = []
+                    response.data.map((item) => {
 
-    getTeamInfo(team) {
-        const index = this.props.teamsId[this.props.teams.findIndex(item => {
-            return item.name === team.name
-        })]
+                        axios.get("https://app.seker.live/fm1/history/" + currentLeagueId + "/" + item.id)
+                            .then((response) => {
+                                let tempPoints = 0
+                                let delta = 0
+                                response.data.forEach(game => {
+                                    let goalsScored = 0
+                                    let goalsConceded = 0
+                                    let home
 
-        axios.get("https://app.seker.live/fm1/history/" + this.props.leagueId + "/" + this.props.teamsId[index]).
-        then((response) => {
-            this.calcEachTeamPoints(response)
-        })
+                                    if (game.homeTeam.id === item.id) {
+                                        home = true
+                                    }else {
+                                        home = false
+                                    }
+                                    game.goals.forEach(goal => {
+                                        if (goal.home === home) {
+                                            goalsScored++
+                                            delta++
+                                        } else {
+                                            goalsConceded++
+                                            delta--
+                                        }
+                                    })
 
-        axios.get("https://app.seker.live/fm1/squad/" + this.props.leagueId + "/" + this.props.teamsId[index]).
-        then((response) => {
+                                    if (goalsScored > goalsConceded) {
+                                        tempPoints+=3
+                                    } else if (goalsScored === goalsConceded) {
+                                        tempPoints++
+                                    }
+                                })
+                                this.tempTeams.push({
+                                    name : item.name,
+                                    id : item.id,
+                                    points : tempPoints,
+                                    goalsDelta : delta
+                                })
 
-        })
-    }*/
-
-    playerNameList = (id) => {
-        let name
-        this.arrPlayerNames = []
-        axios.get("https://app.seker.live/fm1/squad/" + this.props.leagueId + "/" + id)
-            .then((response) => {
-                response.data.forEach(item => {
-                    name = item.firstName + " " + item.lastName
-                    this.arrPlayerNames.push(name)
+                            })
+                    })
+                    this.setState({
+                        teams: this.tempTeams,
+                    })
                 })
-            })
-        this.setState({
-            playerNames: this.arrPlayerNames
-        })
-    }
+        }
 
-    render(){
+
+        /*
+
+
+            playerNameList = (id) => {
+                let name
+                this.arrPlayerNames = []
+                axios.get("https://app.seker.live/fm1/squad/" + this.props.currentLeagueId + "/" + id)
+                    .then((response) => {
+                        response.data.forEach(item => {
+                            name = item.firstName + " " + item.lastName
+                            this.arrPlayerNames.push(name)
+                        })
+                    })
+                this.setState({
+                    playerNames: this.arrPlayerNames
+                },() => {
+                    console.log(this.state.playerNames)
+                })
+            }
+
+            showDetails = (id) => {
+                this.playerNameList(id)
+                return (
+                    <ul>
+                        {
+                            this.state.playerNames.map((item) => {
+                                console.log(item)
+                                return (
+                                    <li key={item}>{item}</li>
+                                )
+                            })
+
+                        }
+                    </ul>
+
+                )
+            }
+
+
+            sortTeams() {
+    let tempTeams = this.state.teams
+    tempTeams  = tempTeams.sort((a, b) => {
+      if (a.points > b.points){
+        return -1
+      }
+      if (a.points  === b.points){
+        if (a.goalsDelta > b.goalsDelta){
+          return -1
+        }
+        if (a.goalsDelta === b.goalsDelta){
+          if (a.name < b.name){
+            return  -1
+          }
+        }
+      }
+    })
+    this.setState ({
+      teams: tempTeams
+    })
+  }
+        */
+
+
+    render() {
         return(
             <div>
-                <table>
-                    <tr>
-                        <th>Points</th>
-                        <th>Goals Delta</th>
-                        <th>Name</th>
-                    </tr>
-                    {
-                        this.props.teams.map((item) => {
-                            return(
-                                <tr /*onClick={this.playerNameList(item.id)}*/>
-                                    <td>
-                                        {item.points}
-                                    </td>
-                                    <td>
-                                        {item.goalsDelta}
-                                    </td>
-                                    <td>
-                                        {item.name}
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
+                <SelectBox responseClick={this.getTeams.bind(this)}/>
+                {
+                    (this.state.teams.length > 0) &&
+                    <table>
+                        <tr>
+                            <th>Points</th>
+                            <th>Goals Delta</th>
+                            <th>Name</th>
+                        </tr>
+                        {
+                            this.state.teams.map((team) => {
+                                return(
+                                    <tr /*onClick={() => {
+                                    this.showDetails(team.id)
+                                }}*/>
+                                        <td>
+                                            {team.points}
+                                        </td>
+                                        <td>
+                                            {team.goalsDelta}
+                                        </td>
+                                        <td>
+                                            {team.name}
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </table>
+                }
+
+                {
 
 
-
-
-                </table>
+                }
             </div>
         )
     }
